5623b2991e6d45a2179549b14ff52e894fcd89e4
Huai-Dan Chang <hchang256@wisc.edu>
2024-03-17 20:24:14 -0500

    moving the snake

--- snake.py
+++ snake.py
@@ -1,13 +1,47 @@
 import pygame
 pygame.init()
-dis=pygame.display.set_mode((400,300))
-pygame.display.update()
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+dis=pygame.display.set_mode((800,600))
 pygame.display.set_caption('Snake game by Dana')
+
 game_over=False
+
+x1 = 300
+y1 = 300
+ 
+# hold the updating values of the x and y coordinates.
+x1_change = 0       
+y1_change = 0
+
+clock = pygame.time.Clock()
+
 while not game_over:
     for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            game_over=True
+        if event.type == pygame.QUIT:
+            game_over = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x1_change = -10
+                y1_change = 0
+            elif event.key == pygame.K_RIGHT:
+                x1_change = 10
+                y1_change = 0
+            elif event.key == pygame.K_UP:
+                y1_change = -10
+                x1_change = 0
+            elif event.key == pygame.K_DOWN:
+                y1_change = 10
+                x1_change = 0
+ 
+    x1 += x1_change
+    y1 += y1_change
+    dis.fill(white)
+    pygame.draw.rect(dis, black, [x1, y1, 10, 10])
+    pygame.display.update()
+    # control how fast the game loop runs, effectively controlling the game's frame rate
+    clock.tick(30)
  
 pygame.quit()
 quit()
